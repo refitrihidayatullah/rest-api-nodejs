9051e69fe020c5325dfac856f44c623aa8695159
refitrihidayatullah <refitrihidayatullah19@gmail.com>
2022-10-27 09:09:12 +0700

    revisi readme.md

---
+++ README.md
@@ -0,0 +1,22 @@
+<!-- book rest api -->
+<!-- refi tri hidayatullah -->
+
+<!-- get all books -->
+
+GET https://app-rest-api-books.herokuapp.com/api/books
+
+<!--  post book-->
+
+POST https://app-rest-api-books.herokuapp.com/api/books
+
+<!-- get by id -->
+
+GET https://app-rest-api-books.herokuapp.com/api/books/id
+
+<!-- edit by id -->
+
+PUT https://app-rest-api-books.herokuapp.com/api/books/id
+
+<!-- delete by id -->
+
+DELETE https://app-rest-api-books.herokuapp.com/api/books/id
--- index.js
+++ index.js
@@ -27,6 +27,10 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }).then(() =
     console.log('something wrong!', error)
 })
 
+// index
+app.get('/', (req, res) => {
+    res.send('<h3 style="text-align:center">Rest Api Book</h3><h5 style="text-align:center">GET all data book => GET "https://app-rest-api-books.herokuapp.com/api/books"</h5><h5 style="text-align:center">GET data book by id => GET "https://app-rest-api-books.herokuapp.com/api/books/id"</h5><h5 style="text-align:center">ADD data book => POST "https://app-rest-api-books.herokuapp.com/api/books"</h5><h5 style="text-align:center">UPDATE data book => PUT "https://app-rest-api-books.herokuapp.com/api/books/id"</h5><h5 style="text-align:center">DELETE data book => DELETE "https://app-rest-api-books.herokuapp.com/api/books/id"</h5><h3 style="text-align:center">Refi Tri Hidayatullah 2022</h3>');
+})
 
 
 
